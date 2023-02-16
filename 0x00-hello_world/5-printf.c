c38a0706275279e4bbd9c7feeafad54b8a91afae
Mary Mtetwa <kutaudziramary123@gmail.com>
2023-02-16 14:06:11 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry Point
+*
+* Return: Always 0 (Succses)
+*/
+
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
-return(0);}
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
