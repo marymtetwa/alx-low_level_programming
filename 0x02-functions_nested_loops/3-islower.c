1de4db9163bfb81b5833d8ddbcf89636f720840b
Mary Mtetwa <kutaudziramary123@gmail.com>
2023-02-22 13:07:52 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+
 /**
- * main - check the code.
- *
- * Return 1 for lowercase code else 0.
+ * _islower - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for lowercase character or 0 for anything else
  */
+
 int _islower(int c)
+
 {
-if(c>=97 && c <= 122)
-{
-return(1);
-}
-return (0);
+
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
 }
 
