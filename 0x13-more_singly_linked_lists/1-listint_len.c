7854844cecee34d6ae9c3d1518d02c538f404018
marymtetwa <kutaudziramary123@gmail.com>
2023-04-04 12:24:08 -0700

    question 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * Question 1 of assignment 0x13.c
+ *listint_len - returns the number of elements in a linked lists
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+ size_t num = 0;
+
+/*using the while loop*/
+
+ while (h)
+ {
+ num++;
+ h = h->next;
+ }
+
+ return (num);
+}
